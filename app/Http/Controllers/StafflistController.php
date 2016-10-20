95bed28a50fd9150d37afb8182e41e14ab143345
cm <xxmail@xmailx.com>
2016-10-20 14:05:04 +0900

    改行などのコードの見た目修正と 一覧画面のソートを降順に

--- app/Http/Controllers/StafflistController.php
+++ app/Http/Controllers/StafflistController.php
@@ -1,29 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
-
 use App\Staff;
+use App\Http\Requests\StaffRequest;
 
 class StafflistController extends Controller
 {
     // 一覧取得
-    public function list()
-    {
-        $stafflist = Staff::all();
+    public function list(){
+        // $stafflist = Staff::all();
+        $stafflist = Staff::orderBy('staff_no', 'desc')->get();
         return view("stafflist.list", compact("stafflist"));
     }
     //詳細取得
-    public function show($id)
-    {
+    public function show($id){
         $staff = Staff::findOrFail($id);
         return view("stafflist.show", compact("staff"));
     }
-    public function create()
-    {
+    // 詳細新規編集
+    public function create(){
         return view("stafflist.create");
     }
     // DBへの書き込み
