54f89a016ea213ccce927d221a73c5923815aa68
cm <xxmail@xmailx.com>
2016-10-20 16:59:28 +0900

    入力チェックのエラー表示部分を共用化

--- resources/views/stafflist/create.blade.php
+++ resources/views/stafflist/create.blade.php
@@ -2,15 +2,7 @@
 
 @section('content')
 
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+@include('errors.form_errors')
 
 {!! Form::open(['url' => 'staff']) !!}
 <table class="table">
