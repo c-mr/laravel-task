be1c40025b18166fbf184518bdc54158468a1496
cm <xxmail@xmailx.com>
2016-10-27 16:10:40 +0900

    新規登録時に同じ日付でも登録者が違うものの入力チェックを外す

--- app/Http/Requests/BodyweightsRequest.php
+++ app/Http/Requests/BodyweightsRequest.php
@@ -4,6 +4,8 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
+use DB;
+
 class BodyweightsRequest extends FormRequest{
     /**
      * Determine if the user is authorized to make this request.
@@ -20,10 +22,31 @@ class BodyweightsRequest extends FormRequest{
      * @return array
      */
     public function rules(){
+
+        $user_id = \Auth::user()->id;
+
+        /**
+         * 自分自身のID以外では同じ日付があっても良いので
+         *
+         */
+        // 自分自身のUSER_IDで同じ日があるかDBチェック
+        $bodyweights_count = DB::table('bodyweights')
+                    ->select(DB::raw('count(*) as count, measure_at'))
+                    ->where('user_id', '=', $user_id)
+                    ->where('measure_at', '=', $this->measure_at)
+                    ->groupBy('measure_at')
+                    ->get();
+
+        // 新規登録かつ、同じ日登録が0件で無ければ入力チェックを追記
+        $validation_measure_at = "";
+        if ( count($bodyweights_count) != 0 ){
+            $validation_measure_at = '|unique:bodyweights,measure_at,'.$this->id;
+        }
+
         return [
             'user_id' => 'required|numeric',
             // 同日、登録出来るのは1回のみ但し、自分以外の登録は除外
-            'measure_at' => 'required|date|unique:bodyweights,measure_at,'.$this->id,
+            'measure_at' => 'required|date'.$validation_measure_at,
             'bodyweight' => 'required|numeric',
         ];
     }
